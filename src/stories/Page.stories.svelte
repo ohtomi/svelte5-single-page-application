376a37ce0df4b661b60df8f657f1b003a95c386d
ohtomi <ohtomi.kenichi@gmail.com>
2024-11-02 22:03:34 +0900

    コードを整形した

--- src/stories/Page.stories.svelte
+++ src/stories/Page.stories.svelte
@@ -1,18 +1,17 @@
 <script module>
-  import { defineMeta } from '@storybook/addon-svelte-csf';
-  import { expect, userEvent, waitFor, within } from '@storybook/test';
-  import Page from './Page.svelte';
-  import { fn } from '@storybook/test';
+    import {defineMeta} from '@storybook/addon-svelte-csf';
+    import {expect, userEvent, waitFor, within} from '@storybook/test';
+    import Page from './Page.svelte';
 
-  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
-  const { Story } = defineMeta({
-    title: 'Example/Page',
-    component: Page,
-    parameters: {
-      // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
-      layout: 'fullscreen',
-    },
-  });
+    // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+    const {Story} = defineMeta({
+        title: 'Example/Page',
+        component: Page,
+        parameters: {
+            // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+            layout: 'fullscreen',
+        },
+    });
 </script>
 
 <Story name="Logged In" play={async ({ canvasElement }) => {
@@ -25,6 +24,6 @@
     const logoutButton = canvas.getByRole('button', { name: /Log out/i });
     await expect(logoutButton).toBeInTheDocument();
   }}
-  />
+/>
 
-<Story name="Logged Out" />
+<Story name="Logged Out"/>

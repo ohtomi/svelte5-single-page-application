a88aaf2be40cf8e5197c761d2d2314ff260d6111
ohtomi <ohtomi.kenichi@gmail.com>
2024-12-18 07:58:19 +0900

    <svelte:head>要素をDOMツリーの冒頭へ移した

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
-<svelte:head>
-    <title>GitHub Repository Search</title>
-</svelte:head>
-
 <script lang="ts">
 import "../app.css";
 
 let { children } = $props();
 </script>
+
+<svelte:head>
+    <title>GitHub Repository Search</title>
+</svelte:head>
 
 <div class="bg-white p-4 dark:bg-gray-800">
     {@render children()}

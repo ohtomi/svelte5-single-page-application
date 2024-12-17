d11f9c0fc2157b2ca031e9a2c650ff85cd81857d
ohtomi <ohtomi.kenichi@gmail.com>
2024-12-18 07:58:40 +0900

    Storybookの設定スクリプトをTypeScript化した

---
+++ .storybook/preview.ts
@@ -0,0 +1,17 @@
+import type { Preview } from "@storybook/svelte";
+
+// TODO css import!
+// import '../src/app.css';
+
+const preview: Preview = {
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+};
+
+export default preview;

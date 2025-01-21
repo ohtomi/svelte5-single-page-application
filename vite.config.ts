fd7657edd48b967518ba782b2ad6040e9beedda5
ohtomi <ohtomi.kenichi@gmail.com>
2025-01-21 21:44:58 +0900

    postcss.config.jsの設定をvite.config.tsへ移した

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { sveltekit } from "@sveltejs/kit/vite";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	plugins: [sveltekit()],
+
+	css: {
+		postcss: {
+			plugins: [tailwindcss, autoprefixer],
+		},
+	},
 
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"],
